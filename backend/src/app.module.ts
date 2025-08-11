89b64ca63a6b37238577dd74f0d0da884e8e6fb4
Alex Faustino <faustino.alex@outlook.com>
2025-08-11 20:08:19 -0300

    ref: update  admin configuration in bootstrap and update .env.example

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,35 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/infra/auth.controller';
-import { AuthService } from './auth/application/auth.service';
-import { AuthUseCase } from './auth/application/auth.use-case';
-import { JwtStrategy } from './auth/application/jwt.strategy';
-import { JwtAuthGuard } from './auth/infra/guards/jwt-auth.guard';
-import { JwtService } from './auth/application/jwt.service';
-import { APP_GUARD } from '@nestjs/core';
-import { PassportModule } from '@nestjs/passport';
-import { LoggerProvider, WinstonLoggerModule } from './logger/winstor.logger';
-import { AuthRepository } from './auth/infra/auth.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { CollaboratorsModule } from './collaborators/collaborator.module';
+import { LoggerProvider } from './logger/winstor.logger';
 
 @Module({
-  imports: [PassportModule, WinstonLoggerModule],
-  controllers: [AppController, AuthController],
-  providers: [
-    AppService,
-    AuthService,
-    AuthUseCase,
-    JwtService,
-    JwtStrategy,
-    LoggerProvider,
-    {
-      provide: 'AuthInterface',
-      useClass: AuthRepository,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: JwtAuthGuard,
-    },
+  imports: [
+    CollaboratorsModule,
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URI || ''),
   ],
+  controllers: [AppController],
+  providers: [AppService, LoggerProvider],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/http-exception.filter';
+import { AuthService } from './auth/application/auth.service';
+import { LoggerService } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  const userService = app.get(AuthService);
+  const logger = app.get<LoggerService>('Logger');
+
+  const adminEmail = process.env.ADMIN_EMAIL;
+  const adminPassword = process.env.ADMIN_PASSWORD;
+
+  if (adminEmail && adminPassword) {
+    const adminExists = await userService.findByEmail(adminEmail);
+    if (!adminExists) {
+      await userService.createAdmin({
+        name: 'Admin',
+        email: adminEmail,
+        password: adminPassword,
+      });
+      logger.log('Admin criado automaticamente');
+    }
+  }
+
   await app.listen(process.env.PORT ?? 4000);
 }
+
 void bootstrap();
