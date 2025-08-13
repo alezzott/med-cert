a2ac139174f06299713bddba50425684984ce208
Alex Faustino <faustino.alex@outlook.com>
2025-08-12 23:51:29 -0300

    feat: refactor logger implementation to use winston directly and enhance logging format

--- backend/src/logger/winstor.logger.ts
+++ backend/src/logger/winstor.logger.ts
@@ -1,18 +1,51 @@
-import { WinstonModule } from 'nest-winston';
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-base-to-string */
+import { LoggerService } from '@nestjs/common';
 import * as winston from 'winston';
 
-export const WinstonLoggerModule = WinstonModule.forRoot({
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.simple(),
-      ),
-    }),
-  ],
+winston.addColors({
+  error: 'bold red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  verbose: 'cyan',
+  debug: 'blue',
+  silly: 'grey',
 });
+
+export const winstonLogger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.colorize({ all: true }),
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.printf(({ timestamp, level, message, context }) => {
+      const nestPrefix = '[Nest]';
+      const ctx = context ? `[${context}]` : '';
+      return `${nestPrefix} ${timestamp}   ${level} ${ctx} ${message}`;
+    }),
+  ),
+  transports: [new winston.transports.Console()],
+});
+
+export class WinstonLoggerService implements LoggerService {
+  log(message: string, context?: string) {
+    winstonLogger.info(message, { context });
+  }
+  error(message: string, trace?: string, context?: string) {
+    winstonLogger.error(message, { trace, context });
+  }
+  warn(message: string, context?: string) {
+    winstonLogger.warn(message, { context });
+  }
+  debug(message: string, context?: string) {
+    winstonLogger.debug(message, { context });
+  }
+  verbose(message: string, context?: string) {
+    winstonLogger.verbose(message, { context });
+  }
+}
 
 export const LoggerProvider = {
   provide: 'Logger',
-  useValue: WinstonLoggerModule,
+  useClass: WinstonLoggerService,
 };
