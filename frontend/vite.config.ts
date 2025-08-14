c8bfaa3949a83937b713bc4533da777dbdc62d67
Alex Faustino <faustino.alex@outlook.com>
2025-08-13 21:06:56 -0300

    feat: update frontend dependencies and enhance project structure

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss  from '@tailwindcss/vite';
+import path from 'path';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '/src'),
+    },
+  },
+});
